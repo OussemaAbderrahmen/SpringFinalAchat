6c66ef7179ec9fc68a91fef4d7194e8a0b24b32d
ahmedbali19 <ahmed.bali@esprit.tn>
2022-10-29 10:53:45 -0400

    commit Ahmed

--- src/main/java/com/esprit/examen/entities/DetailFacture.java
+++ src/main/java/com/esprit/examen/entities/DetailFacture.java
@@ -1,12 +1,11 @@
 package com.esprit.examen.entities;
 
 import java.io.Serializable;
-import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,23 +17,19 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Stock implements Serializable {
+public class DetailFacture implements Serializable {
 	private static final long serialVersionUID = 1L;
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idStock;
-	private String libelleStock;
-	private Integer qte;
-	private Integer qteMin;
-	@OneToMany(mappedBy = "stock")
+	private Long idDetailFacture;
+	private Integer qteCommandee;
+	private float prixTotalDetail;
+	private Integer pourcentageRemise;
+	private float montantRemise;
+	@ManyToOne
+	private Produit produit;
+	@ManyToOne
 	@JsonIgnore
-	private Set<Produit> produits;
-	public Stock(String libelleStock, Integer qte, Integer qteMin) {
-		super();
-		this.libelleStock = libelleStock;
-		this.qte = qte;
-		this.qteMin = qteMin;
-	}
+	Facture facture;
 
 }
