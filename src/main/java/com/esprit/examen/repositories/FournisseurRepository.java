46354b2821f809ab783f268a41f06f49d052787e
Karim Chakroun <79665389+karim-chakroun@users.noreply.github.com>
2022-10-29 16:59:15 +0200

    Revert "commit Ahmed"

--- src/main/java/com/esprit/examen/repositories/FournisseurRepository.java
+++ src/main/java/com/esprit/examen/repositories/FournisseurRepository.java
@@ -2,9 +2,9 @@ package com.esprit.examen.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.esprit.examen.entities.DetailFacture;
+import com.esprit.examen.entities.Fournisseur;
 
 @Repository
-public interface DetailFactureRepository extends JpaRepository<DetailFacture, Long> {
+public interface FournisseurRepository extends JpaRepository<Fournisseur, Long> {
 
 }
