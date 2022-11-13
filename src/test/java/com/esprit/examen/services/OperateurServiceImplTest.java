8ef6d485e6387fb6e1c1643a0ce93e86bf1aee9b
ahmed-debbech <debbech.ahmed@gmail.com>
2022-11-14 00:48:15 +0100

    remove unused imports

--- src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
@@ -7,11 +7,9 @@ import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
